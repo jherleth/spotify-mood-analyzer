6b5cbf6ec55685ada6eb430a405ac3bd13735416
Jackson <jackson.herleth@gmail.com>
2025-08-29 20:33:46 -0500

    Updated basic frontend

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.get("/login", (req, res) => {
 		response_type: "code",
 		client_id: process.env.SPOTIFY_CLIENT_ID,
 		scope,
-		redirect_uri: provess.env.REDIRECT_URI,
+		redirect_uri: process.env.REDIRECT_URI,
 	});
 	res.redirect(`${SPOTIFY_AUTH_URL}?${params}`);
 });
